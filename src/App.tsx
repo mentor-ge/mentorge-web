40c10037aaeb9ac5fd315e3007b208c44eae6e85
George Mdivani <georgemdivani.gm@gmail.com>
2019-07-21 14:50:11 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="container App">
+      <h1 className="my-5">გახდი მენტორი</h1>
+    </div>
+  );
+};
+
+export default App;
